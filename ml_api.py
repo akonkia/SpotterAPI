c93c585ca62bf093a9991ac5106151685d44eb21
Agnieszka Konkolewska <68012357+akonkia@users.noreply.github.com>
2024-04-26 23:47:10 +0200

    Add files via upload

--- ml_api.py
+++ ml_api.py
@@ -5,6 +5,57 @@ import DeepImageSearch
 import json
 from tempfile import NamedTemporaryFile
 
+import sys
+import requests
+
+import os
+cwd = os.getcwd()
+def download_file_from_google_drive(file_id, destination):
+    URL = "https://docs.google.com/uc?export=download&confirm=1"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={"id": file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {"id": file_id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+def main():
+    if len(sys.argv) >= 3:
+        file_id = sys.argv[1]
+        destination = sys.argv[2]
+    else:
+        file_id = "1IExhCApNaUpYZ7kDZZUHdRmBG8s6bHXx"
+        destination = "prediction_model.sav"
+    print(f"download {file_id} to {destination}")
+    download_file_from_google_drive(file_id, destination)
+
+
+if __name__ == "__main__":
+    main()
+
 
 app= FastAPI()
 
