173602e0fac14dc9a6085fce3ba887bbb7975719
Agnieszka Konkolewska <68012357+akonkia@users.noreply.github.com>
2024-04-27 07:38:42 +0200

    Add files via upload

--- ml_api.py
+++ ml_api.py
@@ -8,11 +8,18 @@ from tempfile import NamedTemporaryFile
 import sys
 import requests
 
-from zipfile import ZipFile
-zf = ZipFile('prediction_model.sav.zip', 'r')
-zf.extractall()
-zf.close()
+def concatenate_files(input_files, output_file):
+    with open(output_file, 'wb') as output:
+        for input_file in input_files:
+            with open(input_file, 'rb') as f:
+                output.write(f.read())
 
+input_files = ['model_chunk_0', 'model_chunk_1', 'model_chunk_2',
+               'model_chunk_3','model_chunk_4','model_chunk_5','model_chunk_6',
+               'model_chunk_7'] 
+output_file = 'prediction_model.sav'  # Name of the concatenated output file
+
+concatenate_files(input_files, output_file)
 
 app= FastAPI()
 
@@ -22,7 +29,7 @@ class model_input(BaseModel):
 import uuid
 
 #load model
-st = pickle.load(open('prediction_model.sav.zip', 'rb'))
+st = pickle.load(open('prediction_model.sav', 'rb'))
 
 # @app.post('/image_prediction')
 # async def image_pred(file: UploadFile = File()):
@@ -42,8 +49,9 @@ async def image_pred(file: UploadFile = File(...)):
         image_path = file_on_disk.name
         print(image_path)
         new_image = image_path 
-        st.add_images_to_index([new_image])
-        prediction = st.get_similar_images(new_image, number_of_images=4)
+        # st.add_images_to_index([new_image])
+        # prediction = st.get_similar_images(new_image, number_of_images=4)
+        prediction=image_path
         print(prediction)
         return(prediction)
 
@@ -74,3 +82,25 @@ async def image_pred(file: UploadFile = File(...)):
 #         file.file.close()
 
 #     return {"message": f"Successfully uploaded {file.filename}"}
+    
+
+# import os
+
+# def split_file(input_file, output_prefix, chunk_size):
+#     with open(input_file, 'rb') as f:
+#         chunk_num = 0
+#         while True:
+#             chunk = f.read(chunk_size)
+#             if not chunk:
+#                 break
+#             with open(f'{output_prefix}_{chunk_num}', 'wb') as chunk_file:
+#                 chunk_file.write(chunk)
+#             chunk_num += 1
+
+# input_file = 'prediction_model.sav'  # Replace 'your_input_file.txt' with the path to your input file
+# output_prefix = 'model_chunk'  # Output file prefix
+# chunk_size = 25 * 1024 * 1024  # 100 MB chunk size
+
+# split_file(input_file, output_prefix, chunk_size)
+
+
