1b3053a9816b28af497e61f4f14991068870a94e
Agnieszka Konkolewska <68012357+akonkia@users.noreply.github.com>
2024-05-01 18:28:25 +0100

    Improved api response

--- api_implementation.py
+++ api_implementation.py
@@ -1,11 +1,23 @@
-import json
 import requests
+import argparse
+
+parser = argparse.ArgumentParser(description='Return a list of most similar images')
+parser.add_argument('filename', metavar='file', type=str, nargs='+',
+                    help='a filename for parsing')
+
+args = parser.parse_args()
+
+file_path = args.filename[0]
 
 url = 'http://127.0.0.1:8000/image_prediction'
 
 files = {
-    'file': open(r'52424_1_MN.jpg', 'rb'),
+    'file': open(file_path, 'rb'),
 }
-
+    
 response = requests.post(url, files=files)
-print(response)
+print(response.text)
+
+
+#usage e.g.: python api_implementation.py '52424_1_MN.jpg'
+
--- ml_api.py
+++ ml_api.py
@@ -4,7 +4,7 @@ import pickle
 import DeepImageSearch
 import json
 from tempfile import NamedTemporaryFile
-
+import re
 import sys
 import requests
 
@@ -27,80 +27,42 @@ class model_input(BaseModel):
     file: UploadFile = File()
 
 import uuid
+import copy 
+import shutil
 
-#load model
-st = pickle.load(open('prediction_model.sav', 'rb'))
+input_files = ['model_chunk_0', 'model_chunk_1', 'model_chunk_2',
+               'model_chunk_3','model_chunk_4','model_chunk_5','model_chunk_6',
+               'model_chunk_7'] 
+output_file = 'prediction_model.sav'  # Name of the concatenated output file
+concatenate_files(input_files, output_file)
 
-# @app.post('/image_prediction')
-# async def image_pred(file: UploadFile = File()):
-#     file.filename = f"{uuid.uuid4()}.jpg"
-#     #contents = await file.read()  # <-- Important!
-#     new_image = file.filename 
-#     st.add_images_to_index([new_image])
-#     prediction = st.get_similar_images(new_image, number_of_images=4)
-#     return(prediction)
-
+import os
 @app.post('/image_prediction')
 async def image_pred(file: UploadFile = File(...)):
+    #get the clean index, without newly added features from parsed files
+    shutil.copy("./metadata-files/inception_v4.tf_in1k/image_data_features0.pkl","./metadata-files/inception_v4.tf_in1k/image_data_features.pkl")
+    shutil.copy("./metadata-files/inception_v4.tf_in1k/image_features_vectors0.idx","./metadata-files/inception_v4.tf_in1k/image_features_vectors.idx")
+    fresh = pickle.load(open('prediction_model.sav', 'rb'))
+    st = copy.deepcopy(fresh)
+    print("Loaded model")
+    print("Parsing file...")
     file_suffix = "".join(file.filename.partition(".")[1:])
     with NamedTemporaryFile(mode="w+b", suffix=file_suffix) as file_on_disk:
         file_contents = await file.read()
         file_on_disk.write(file_contents)
         image_path = file_on_disk.name
-        print(image_path)
         new_image = image_path 
-        # st.add_images_to_index([new_image])
-        # prediction = st.get_similar_images(new_image, number_of_images=4)
-        prediction=image_path
-        print(prediction)
-        return(prediction)
+        st.add_images_to_index([new_image])
+        prediction = st.get_similar_images(new_image, number_of_images=10)
+        print(str(prediction))
+        integers_list = [int(match.group()) for match in re.finditer(r'\b\d+\b', str(prediction))]
+        id_dict = pickle.load(open('id_list.pkl', 'rb'))
+        found_ids = []
+        # Check each integer in the list
+        for integer in integers_list:
+            if integer in id_dict:
+                found_ids.append(id_dict[integer])
 
-# app = FastAPI()
+        return(found_ids)
 
-
-# @app.post("/images/")
-# async def create_upload_file(file: UploadFile = File(...)):
-
-#     file.filename = f"{uuid.uuid4()}.jpg"
-#     contents = await file.read()  # <-- Important!
-
-#     #db.append(contents)
-
-#     return {"filename": file.filename}
-
-
-
-# @app.post("/upload")
-# def upload(file: UploadFile = File(...)):
-#     try:
-#         contents = file.file.read()
-#         with open(file.filename, 'wb') as f:
-#             f.write(contents)
-#     except Exception:
-#         return {"message": "There was an error uploading the file"}
-#     finally:
-#         file.file.close()
-
-#     return {"message": f"Successfully uploaded {file.filename}"}
-    
-
-# import os
-
-# def split_file(input_file, output_prefix, chunk_size):
-#     with open(input_file, 'rb') as f:
-#         chunk_num = 0
-#         while True:
-#             chunk = f.read(chunk_size)
-#             if not chunk:
-#                 break
-#             with open(f'{output_prefix}_{chunk_num}', 'wb') as chunk_file:
-#                 chunk_file.write(chunk)
-#             chunk_num += 1
-
-# input_file = 'prediction_model.sav'  # Replace 'your_input_file.txt' with the path to your input file
-# output_prefix = 'model_chunk'  # Output file prefix
-# chunk_size = 25 * 1024 * 1024  # 100 MB chunk size
-
-# split_file(input_file, output_prefix, chunk_size)
-
-
+#uvicorn ml_api:app   - use to start the server with api
